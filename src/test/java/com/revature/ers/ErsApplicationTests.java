f4d796fd3555740ca6481a98e0c3199d7079b124
Jiwon Shim <jshim0209@hotmail.com>
2022-10-22 16:34:57 -0400

    caught more code smells and fixed

--- src/test/java/com/revature/ers/ErsApplicationTests.java
+++ src/test/java/com/revature/ers/ErsApplicationTests.java
@@ -1,13 +1,29 @@
 package com.revature.ers;
 
+import com.revature.ers.controller.AuthenticationController;
+import com.revature.ers.controller.ReimbursementController;
+import com.revature.ers.controller.UserController;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ErsApplicationTests {
 
+	@Autowired
+	private AuthenticationController authenticationController;
+
+	@Autowired
+	private ReimbursementController reimbursementController;
+
+	@Autowired
+	private UserController userController;
+
 	@Test
 	void contextLoads() {
+		Assertions.assertThat(authenticationController).isNotNull();
+		Assertions.assertThat(reimbursementController).isNotNull();
+		Assertions.assertThat(userController).isNotNull();
 	}
-
 }
